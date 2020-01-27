8f43f32f768a0fe3f00d0ec35276f287c3b42a12
Jose Amesquita and Mariam Alaeddin <mariam@>
2020-01-27 09:32:29 -0800

    create two functions to find and delete contacts

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,17 @@
 
 function AddressBook() {
     this.contacts = [];
+    this.currentId = 0;
 }
 
 AddressBook.prototype.addContact = function(contact){
+    contact.id = this.assignId();
     this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function(){
+    this.currentId += 1;
+    return this.currentId;
 }
 
 // business logic for Contacts
@@ -22,6 +29,23 @@ Contact.prototype.fullName = function() {
     return this.firstName + " " + this.lastName;
 }
 
+AddressBook.prototype.findContact = function(id) {
+    for (var i = 0; i < this.contacts.length; i++){
+        if (this.contacts[i].id == id) {
+            return this.contacts[i];
+        }
+    };
+    return false;
+}
+AddressBook.prototype.deleteContact = function(id) {
+    for (var i = 0; i < this.contacts.length; i++) {
+        if (this.contacts[i].id == id) {
+            delete this.contacts[i];
+            return true;
+        }
+    }
+}
+
 //test.fullName();
 
 var addressBook = new AddressBook();
@@ -29,5 +53,7 @@ var contact = new Contact("Ada", "Lovelace","503-555-0100");
 var contact2 = new Contact("Grace", "Hooper", "503-555-0199");
 addressBook.addContact(contact);
 addressBook.addContact(contact2);
+
+
 
 //addressBook.contacts[0].fullName;
