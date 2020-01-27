8cdb58ec0a05492b2abadd7eaa1ef5ed2616113d
Jose Amesquita and Mariam Alaeddin <mariam@>
2020-01-27 14:27:11 -0800

    1. add contacts to address book

--- js/scripts.js
+++ js/scripts.js
@@ -1,99 +1,73 @@
-// business logic for AddressBook
-
+// Business Logic for AddressBook ---------
 function AddressBook() {
-    this.contacts = [];
-    this.currentId = 0;
+  this.contacts = [],
+  this.currentId = 0
 }
 
-AddressBook.prototype.addContact = function(contact){
-    contact.id = this.assignId();
-    this.contacts.push(contact);
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts.push(contact);
 }
 
-AddressBook.prototype.assignId = function(){
-    this.currentId += 1;
-    return this.currentId;
-}
-
-// AddressBook.prototype.UpdateContact = function(firstName, lastName, phone){
-//     this.firstName = firstName;
-//     this.lastName = lastName;
-//     this.phone = phone; 
-// }
-
-AddressBook.prototype.updateContact = function(firstName, lastName, phone){
-    contact.firstName = this.UpdateContact();
-    this.contacts.push(contact);
-    contact.lastName = this.UpdateContact();
-    this.contacts.push(contact);
-    contact.phone = this.UpdateContact();
-    this.contacts.push(contact);
-}
-
-// business logic for Contacts
-
-function Contact(firstName, lastName, phone) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.phone = phone;
-}
-
-function UpdateContact(firstName, lastName, phone) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.phone = phone;
-}
-
-var test = new Contact ("Jose", "Amesquita", "503.555.5555");
-
-Contact.prototype.fullName = function() {
-    return this.firstName + " " + this.lastName;
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
 }
 
 AddressBook.prototype.findContact = function(id) {
-    for (var i = 0; i < this.contacts.length; i++){
-        if (this.contacts[i].id == id) {
-            return this.contacts[i];
-        }
-    };
-    return false;
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
+    }
+  };
+  return false;
 }
+
 AddressBook.prototype.deleteContact = function(id) {
-    for (var i = 0; i < this.contacts.length; i++) {
-        if (this.contacts[i].id == id) {
-            delete this.contacts[i];
-            return true;
-        }
-    } 
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  };
+  return false;
 }
-// AddressBook.prototype.updateContact = function(id){
-//     for (var i = 0; i < this.contacts.length; i++){
-//         if (this.contacts[i].id == id)
-//         delete this.contacts[i];
-//         return (updateContact)
-    
 
-Contact.prototype.UpdateContact = function(firstName, lastName, phone) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.phone = phone; 
+// Business Logic for Contacts ---------
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName,
+  this.lastName = lastName,
+  this.phoneNumber = phoneNumber
 }
-// AddressBook.prototype.updateContact = function(firstName, lastName, phone){
-//     for (var i=0; i < this.contacts.length; i++) {
-//         if (this.contacts[i].firstName == firstName){
-//             this.contacts[i].firstName == 
-//         }
-//     }
-// } 
 
-//test.fullName();
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
 
+// User Interface Logic ---------
 var addressBook = new AddressBook();
-var contact = new Contact("Ada", "Lovelace","503-555-0100");
-var contact2 = new Contact("Grace", "Hooper", "503-555-0199");
-addressBook.addContact(contact);
-addressBook.addContact(contact2);
 
+function displayContactDetails(addressBookToDisplay) {
+  var contactsList = $("ul#contacts");
+  var htmlForContactInfo = "";
+  addressBookToDisplay.contacts.forEach(function(contact) {
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + " " + contact.phoneNumber +"</li>";
+  });
+  contactsList.html(htmlForContactInfo);
+};
 
-
-//addressBook.contacts[0].fullName;
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedPhoneNumber = $("input#new-phone-number").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    addressBook.addContact(newContact);
+    displayContactDetails(addressBook);
+  })
+})
